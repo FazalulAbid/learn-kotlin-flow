6936702391f207be0a8305eb17888930aa9bfcaf
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-13 19:16:38 +0530

    State flow in jetpack compose.

--- app/src/main/java/com/fifty/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/fifty/kotlinflows/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -19,13 +19,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             KotlinFlowsTheme {
                 val viewModel = viewModel<MainViewModel>()
-                val time = viewModel.countDownFlow.collectAsState(initial = 10)
+                val count = viewModel.stateFlow.collectAsState(initial = 0)
                 Box(modifier = Modifier.fillMaxSize()) {
-                    Text(
-                        text = time.value.toString(),
-                        fontSize = 30.sp,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
+                    Button(onClick = {
+                        viewModel.incrementCounter()
+                    }) {
+                        Text(
+                            text = "Counter: ${count.value}",
+                        )
+                    }
                 }
             }
         }
