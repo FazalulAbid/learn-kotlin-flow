777d5f3eef7a278f703f40840cb2ee41b8d8253b
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-13 18:47:13 +0530

    Flow collect and collect Latest exercise.

--- app/src/main/java/com/fifty/kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/fifty/kotlinflows/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.fifty.kotlinflows
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -14,6 +17,19 @@ class MainViewModel : ViewModel() {
             delay(1000L)
             currentValue--
             emit(currentValue)
+        }
+    }
+
+    init {
+        collectFlow()
+    }
+
+    // Collect
+    private fun collectFlow() {
+        viewModelScope.launch {
+            countDownFlow.collect { time ->
+                println("The current time is $time")
+            }
         }
     }
 }
